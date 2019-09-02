24e7adcd716bfc9aa9c3527ca31b217402e5a333
Blissz <arielgeometrydash@gmail.com>
2019-09-01 22:04:08 -0300

    weon
    xd

---
+++ sboxgdps/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace sboxgdps
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{	
+			// About this: https://discord.gg/NVMkrhU
+			Console.Write("pls wait work in progress lol XD press F for exit");
+			while (Console.ReadKey().Key != ConsoleKey.F) {}
+		}
+	}
+}
